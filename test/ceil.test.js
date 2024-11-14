8c95cf43b9dc3122aca4f8505de6b3836e563e72
Minja Survonen <minja.survonen@tuni.fi>
2024-11-14 15:45:49 +0200

    Unit tests for ceil.js

---
+++ test/ceil.test.js
@@ -0,0 +1,22 @@
+import ceil from '../src/ceil.js';
+
+describe('ceil function', () => {
+    it('Rounds up to bigger integer', () => {
+        expect(ceil(5.010)).toBe(6); // 6 is the next integerto round up
+    });
+    it('rounds with two decimals', () => {
+        expect(ceil(20.002,2)).toBe(20.01) // round up with two decimals
+    });
+    it('rounds up with 1 decimal', () => {
+        expect(ceil(10.20001,1)).toBe(10.3) // Rounds up with one decimal
+    });
+
+    it('Rounds up to next hundred', () => {
+        expect(ceil(2024,-2)).toBe(2100) // The next hundred after 2000
+    });
+
+
+    it('Rounds up to next ten', () => {
+        expect(ceil(2024,-1)).toBe(2030) // The next hundred after 2000
+    });
+});
